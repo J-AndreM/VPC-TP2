288b2d02a9dedb1da037139139f58395e699f11a
J-AndreM <joao.andrem@outlook.com>
2017-02-26 00:29:39 +0000

    imagens

--- Parte_II.m
+++ Parte_II.m
@@ -11,21 +11,50 @@ B_lapla = imfilter(I,h_lapla);
 B_log   = imfilter(I,h_log);
 BI = edge(I,'Canny');
 % ver resultado
+figure('Name','Resultado da Filtragem')
+subplot(2,2,1)
 imshow(B_sobel)
-imshow(B_lapla)
-imshow(B_log)
-imshow(BI)
+title('sobel');
 
-%
+subplot(2,2,2)
+imshow(B_lapla)
+title('laplacian');
+
+subplot(2,2,3)
+imshow(B_log)
+title('log');
+
+subplot(2,2,4)
+imshow(BI)
+title('Canny');
+
+
+
+
+
+%%
+
+% detetar arestas
+figure('Name','Detectores de Arestas')
+subplot(2,2,1)
 BW = edge(I,'Canny');
-figure('Name','edge com Canny')
 imshow(BW)
+title('Canny');
+%
+subplot(2,2,2)
 BW = edge(I,'Sobel');
-figure('Name','edge com Sobel')
 imshow(BW)
+title('Sobel');
+%
+subplot(2,2,3)
 BW = edge(I,'log');
-figure('Name','edge com log')
 imshow(BW)
-BW = edge(I,'zerocross',0.1,h_log);
-figure('Name','edge com zerocross')
+title('log');
+%
+subplot(2,2,4)
+BW = edge(I,'zerocross');
 imshow(BW)
+title('zerocross');
+
+
+
