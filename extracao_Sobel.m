1eebaaee4a85672d969ed485fe78165fcd29ef02
Nitomarques <helder.marques.c@gmail.com>
2017-02-26 23:55:33 +0000

    add

---
+++ extracao_Sobel.m
@@ -0,0 +1,24 @@
+function Im = extracao_Sobel(I, limiar)
+
+s = size(I);
+% Máscara de Sobel
+h_sobel = fspecial('sobel');
+Ix=imfilter(img, h_sobel, 'conv');
+Iy=imfilter(img, h_sobel', 'conv');
+
+
+
+Im = zeros(s(1),s(2), 'uint8');
+
+for j = 1:s(2)   
+    for i = 1:s(1)
+        G = sqrt(double(Ix(l,c))^2 + double(Iy(l,c))^2);       
+        if G > limiar
+            % Coloca a branco se deteta contornos
+            Im(i,j) = 255;
+        else
+            % Se não, coloca a preto
+            Im(i,j) = 0;
+        end
+    end  
+end 
